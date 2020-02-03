9e34ba30180b3148fc0e828104811342d8c7ac25
Sosuke Suzuki <aosukeke@gmail.com>
2020-02-03 10:29:59 +0900

    Initial Commit

---
+++ gatsby-config.js
@@ -0,0 +1,8 @@
+/* eslint-disable */
+module.exports = {
+  siteMetadata: {
+    title: "カラオケ居酒屋 風歌",
+    description: "カラオケ居酒屋 風歌"
+  },
+  plugins: [`gatsby-plugin-styled-components`, `gatsby-plugin-react-helmet`]
+};
---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+import Helmet from "react-helmet";
+
+const Heading1 = styled.h1`
+  color: blue;
+`;
+export default () => (
+  <div>
+    <Heading1>カラオケ居酒屋 風歌</Heading1>
+  </div>
+);
